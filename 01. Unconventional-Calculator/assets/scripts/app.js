f29850fa5ee80266d59f33c4a08960aef308400f
xoraus <imsafay@gmail.com>
2020-01-02 22:45:06 +0530

    L16-com

--- 01. Unconventional-Calculator/assets/scripts/app.js
+++ 01. Unconventional-Calculator/assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert('this does works');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert('The result is ' + result);
+}
+
+add(2,4);
+
+currentResult = (currentResult + 10) * 3 /2 - 1;
+
+let calculationDescription = `(${defaultResult} + 10) * 3 /2 -1`;
+
+outputResult(currentResult,calculationDescription);
