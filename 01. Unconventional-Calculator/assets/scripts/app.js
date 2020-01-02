ec723d407ed808b60f9a2c73c97e0c5235126bfb
xoraus <imsafay@gmail.com>
2020-01-02 23:09:33 +0530

    working-add-27

--- 01. Unconventional-Calculator/assets/scripts/app.js
+++ 01. Unconventional-Calculator/assets/scripts/app.js
@@ -1,15 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    alert('The result is ' + result);
+function add() {
+    currentResult = currentResult + +userInput.value;
+    outputResult(currentResult, ' ');
 }
 
-add(2,4);
 
-currentResult = (currentResult + 10) * 3 /2 - 1;
 
-let calculationDescription = `(${defaultResult} + 10) * 3 /2 -1`;
+addBtn.addEventListener('click', add);
 
-outputResult(currentResult,calculationDescription);
